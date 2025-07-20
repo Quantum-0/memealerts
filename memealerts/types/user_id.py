729ba701f46008852402dbe7ce06ba1ed7d7876b
Anton Kurenkov <quantum0@mail.ru>
2025-07-20 05:14:37 +0300

    fix: async client fix encoding error

--- memealerts/types/user_id.py
+++ memealerts/types/user_id.py
@@ -7,7 +7,13 @@ class UserID(RootModel[Annotated[str, StringConstraints(pattern="^[0-9a-f]{24}$"
     def __init__(self, user_id: str) -> None:
         super().__init__(user_id)
 
+    def __str__(self):
+        return self.root
+
 
 class StickerID(RootModel[Annotated[str, StringConstraints(pattern="^[0-9a-f]{24}$")]]):
     def __init__(self, user_id: str) -> None:
         super().__init__(user_id)
+
+    def __str__(self):
+        return self.root
